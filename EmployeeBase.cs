216729e49365b01d9a60e40bb54bc8b3b695002f
LAPTOP-JDQTLJL4\ASUS <phutuduylanhdao@gmail.com>
2022-05-19 12:59:22 +0700

    Add project files.

---
+++ EmployeeBase.cs
@@ -0,0 +1,16 @@
+﻿namespace PRN.SE623
+{
+    public class EmployeeBase
+    {
+        private int _id;
+        private string _name;
+
+        public double Salary { get; set; }
+    
+        public int id { get => _id; set => _id = value; }
+
+        public String name { get => _name; set => _name = value; }
+    
+        public String NameofCompany { get => _name; }
+    }
+}
